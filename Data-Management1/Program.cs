6b4b07b95d369b4e596c056b265519e15e17233c
aelathy <112655204+aelathy@users.noreply.github.com>
2022-10-20 13:26:50 -0600

    Update Program.cs

--- Data-Management1/Program.cs
+++ Data-Management1/Program.cs
@@ -13,7 +13,8 @@ products.Add(new Product("Laptop", "Acer", 800));
 products.Add(new Product("Phone", "Samsung", 1000));
 products.Add(new Product("Phone", "Apple", 1000));
 products.Add(new Product("Monitor", "Asus", 300));
-
+// Temp Shopping List
+List<Product> ShoppingList = new List<Product>();
 
 bool loop = true;
 while (loop)
@@ -68,16 +69,22 @@ while (loop)
     }
     else if (menuOption == "2")
     {
+        bool result = false;
         // Implement Search Program
         Console.Write("Search by Brand: ");
-        string brandSearch = Console.ReadLine();
+        string brandSearch = Console.ReadLine().ToLower();
         // First letters of the product brand match the letters type (left to right)
         for (int i = 0; i < products.Count; i++)
         {
-            if (brandSearch == products[i].Brand)
+            if (brandSearch == products[i].Brand.ToLower())
             {
+                result = true;
                 Console.WriteLine($"{products[i].Type} {products[i].Brand} ${products[i].Price}");
             }
+        }
+        if (!result)
+        {
+            Console.WriteLine("Product not found.");
         }
     }
     else if (menuOption == "3")
@@ -89,15 +96,56 @@ while (loop)
         // Add Product to Shopping List
         Console.WriteLine("Enter the type & brand of the product you want to ADD:");
         Console.Write("Type: ");
-        string addType = Console.ReadLine();
+        string addType = Console.ReadLine().ToLower();
         Console.Write("Brand: ");
-        string addBrand = Console.ReadLine();
+        string addBrand = Console.ReadLine().ToLower();
+        // for (int i = 0; i <= ShoppingList.Count; i++)
+        // {
+        //     if (addType != ShoppingList[i].Type.ToLower() && addBrand != ShoppingList[i].Brand.ToLower())
+        //     {
+        //         for (int j = 0; j < products.Count; j++)
+        //         {
+        //             if (addType == products[j].Type.ToLower() && addBrand == products[j].Brand.ToLower())
+        //             {
+        //                 ShoppingList.Add(products[j]);
+        //                 Console.WriteLine("Added to shopping list.");
+        //             }
+        //             else
+        //             {
+        //                 Console.WriteLine("broek");
+        //             }
+        //         }
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine("Already in Shopping List.");
+        //     }
+        // }
+
         for (int i = 0; i < products.Count; i++)
         {
-            if (addType == products[i].Type && addBrand == products[i].Brand)
+            if (addType == products[i].Type.ToLower() && addBrand == products[i].Brand.ToLower())
             {
+                foreach (Product item in ShoppingList)
+                {
+                    if (addType != item.Type.ToLower() && addBrand != item.Brand.ToLower())
+                    {
+                        ShoppingList.Add(products[i]);
+                        Console.WriteLine("Product added to shopping list.");
+                    }
+                }
             }
         }
+
+
+        // PLO
+        // for (int i = 0; i < products.Count; i++)
+        // {
+        //     if (addType == products[i].Type.ToLower() && addBrand == products[i].Brand.ToLower())
+        //     {
+        //         ShoppingList.Add(products[i]);
+        //     }
+        // }
     }
     else if (menuOption == "5")
     {
@@ -117,6 +165,10 @@ while (loop)
     else if (menuOption == "6")
     {
         // Display Shopping Cart
+        for (int i = 0; i < ShoppingList.Count; i++)
+        {
+            Console.WriteLine($"{ShoppingList[i].Type} {ShoppingList[i].Brand} ${ShoppingList[i].Price}");
+        }
     }
     else if (menuOption == "7")
     {
